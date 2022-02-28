201473c44e130201671c7aa910bf5abeafe17f06
tina <pokemon0850110@gmail.com>
2022-02-28 21:00:07 +0800

    [MOD]異步回調執行

--- js_promise/封裝promise_1/promise.js
+++ js_promise/封裝promise_1/promise.js
@@ -1,51 +1,73 @@
 //聲明構造函數
-function Promise(executor){
+function Promise(executor) {
     //添加屬性
-    this.PromiseState='pending';
-    this.PromiseResult=null;
+    this.PromiseState = 'pending';
+    this.PromiseResult = null;
+
+    //聲明屬性
+    this.callback = {};
 
     //保存實例對象的this的值
-    const self=this;
+    const self = this;
     //resolve
-    function resolve(data){
+    function resolve(data) {
         //確保狀態只能更改一次
         if (self.PromiseState !== 'pending') return;
 
         //1.修改對象的狀態(promiseState)
-        self.PromiseState='fulfilled';
+        self.PromiseState = 'fulfilled';
 
         //2.設置對象結果值(promiseResult)
-        self.PromiseResult=data;
+        self.PromiseResult = data;
+
+        //調用成功的回調函數
+        if(self.callback.onResolved){
+            self.callback.onResolved(data);
+        }
     }
     //reject
-    function reject(data){
+    function reject(data) {
         //確保狀態只能更改一次
         if (self.PromiseState !== 'pending') return;
-        
+
         //1.修改對象的狀態(promiseState)
-        self.PromiseState='reject';
+        self.PromiseState = 'reject';
 
         //2.設置對象結果值(promiseResult)
-        self.PromiseResult=data;
+        self.PromiseResult = data;
+
+        //調用失敗的回調函數
+        if(self.callback.onReject){
+            self.callback.onReject(data);
+        }
 
     }
-    try{
+    try {
         //同步調用
-        executor(resolve,reject);
-    }catch(e){
+        executor(resolve, reject);
+    } catch (e) {
         //修改promise 對象狀態為失敗
         reject(e);
     }
-    
+
 }
 
 //添加then方法 因為沒有then方法 直接呼叫會報錯(p.then is not a function)
-Promise.prototype.then=function(onResolved,onReject){
-    if (this.PromiseState ==='fulfilled'){
+Promise.prototype.then = function (onResolved, onReject) {
+    if (this.PromiseState === 'fulfilled') {
         onResolved(this.PromiseResult);
     }
-    if (this.PromiseState ==='reject'){
+    if (this.PromiseState === 'reject') {
         onReject(this.PromiseResult);
+    }
+
+    //判斷pending狀態
+    if (this.PromiseState === 'pending') {
+        //保存回調函數
+        this.callback = {
+            onResolved: onResolved,
+            onReject: onReject
+        }
     }
 
 }
